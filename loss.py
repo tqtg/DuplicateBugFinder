0dacb95d898945e4fa3c7f5ea38d044d9762c72d
QuangPham <quangg2012@gmail.com>
2018-04-02 15:30:44 +0800

    Add files via upload

---
+++ loss.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pdb
+class MarginLoss(torch.nn.Module):
+    def __init__(self, margin=1.0):
+        super(MarginLoss, self).__init__()
+        self.margin = margin
+        self.cos = nn.CosineSimilarity(dim=1, eps = 1e-6)
+    def forward(self, x, x_pos, x_neg):
+        fb1 = self.cos(x, x_pos)
+        fb2 = self.cos(x, x_neg)
+        loss = self.margin - fb1 + fb2
+        return F.relu(loss).sum()
