df0af92bb4f767445336c39e66c2ade80e2c92e8
phquang <quangg2012@gmail.com>
2018-04-03 18:27:23 +0800

    update

--- proposed.py
+++ proposed.py
@@ -1,69 +1,114 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-
-class textCNN(torch.nn.Module):
-  def __init__(self, args):
-    super(textCNN, self).__init__()
-    self.word_embed = nn.Embedding(args.n_words, args.word_dim)
-    self.char_embed = nn.Embedding(args.n_chars, args.char_dim)
-
-    self.word_conv3 = nn.Conv1d(args.word_dim, args.n_filters, 3)
-    self.word_conv4 = nn.Conv1d(args.word_dim, args.n_filters, 4)
-    self.word_conv5 = nn.Conv1d(args.word_dim, args.n_filters, 5)
-
-    self.char_conv3 = nn.Conv1d(args.char_dim, args.n_filters, 3)
-    self.char_conv4 = nn.Conv1d(args.char_dim, args.n_filters, 4)
-    self.char_conv5 = nn.Conv1d(args.char_dim, args.n_filters, 5)
-
-
-  def forward(self, x):
-    w_emb = self.word_embed(x[0]).transpose(1, 2)
-    c_emb = self.char_embed(x[1]).transpose(1, 2)
-
-    w_conv3 = F.relu(self.word_conv3(w_emb))
-    w_conv3 = F.max_pool1d(w_conv3, kernel_size=w_conv3.size()[-1])
-    w_conv4 = F.relu(self.word_conv4(w_emb))
-    w_conv4 = F.max_pool1d(w_conv4, kernel_size=w_conv4.size()[-1])
-    w_conv5 = F.relu(self.word_conv5(w_emb))
-    w_conv5 = F.max_pool1d(w_conv5, kernel_size=w_conv5.size()[-1])
-
-    c_conv3 = F.relu(self.char_conv3(c_emb))
-    c_conv3 = F.max_pool1d(c_conv3, kernel_size=c_conv3.size()[-1])
-    c_conv4 = F.relu(self.char_conv4(c_emb))
-    c_conv4 = F.max_pool1d(c_conv4, kernel_size=c_conv4.size()[-1])
-    c_conv5 = F.relu(self.char_conv5(c_emb))
-    c_conv5 = F.max_pool1d(c_conv5, kernel_size=c_conv5.size()[-1])
-
-    w_features = torch.cat([w_conv3, w_conv4, w_conv5], -2)[:, :, 0]
-    c_features = torch.cat([c_conv3, c_conv4, c_conv5], -2)[:, :, 0]
-    tmp = w_features + c_features
-    return tmp
-
+import pdb
+from tqdm import tqdm
+from data_generator import *
 
 class Net(torch.nn.Module):
   def __init__(self, args):
     super(Net, self).__init__()
-    self.short_desc_CNN = textCNN(args)
-    self.long_desc_CNN = textCNN(args)
+    self.word_embed = nn.Embedding(args.n_words, args.word_dim)
+    self.char_embed = nn.Embedding(args.n_chars, args.char_dim)
+
+    self.word_conv3_short = nn.Conv1d(args.word_dim, args.n_filters, 3, padding = 3)
+    self.word_conv4_short = nn.Conv1d(args.word_dim, args.n_filters, 4, padding = 3)
+    self.word_conv5_short = nn.Conv1d(args.word_dim, args.n_filters, 5, padding = 3)
+
+    self.char_conv3_short = nn.Conv1d(args.char_dim, args.n_filters, 3, padding = 3)
+    self.char_conv4_short = nn.Conv1d(args.char_dim, args.n_filters, 4, padding = 3)
+    self.char_conv5_short = nn.Conv1d(args.char_dim, args.n_filters, 5, padding = 3)
+
+    self.word_conv3_long = nn.Conv1d(args.word_dim, args.n_filters, 3, padding = 3)
+    self.word_conv4_long = nn.Conv1d(args.word_dim, args.n_filters, 4, padding = 3)
+    self.word_conv5_long = nn.Conv1d(args.word_dim, args.n_filters, 5, padding = 3)
+
+    self.char_conv3_long = nn.Conv1d(args.char_dim, args.n_filters, 3, padding = 3)
+    self.char_conv4_long = nn.Conv1d(args.char_dim, args.n_filters, 4, padding = 3)
+    self.char_conv5_long = nn.Conv1d(args.char_dim, args.n_filters, 5, padding = 3)
 
     self.prop_MLP = nn.Sequential(nn.Linear(args.n_prop, 256), nn.ReLU(),
                                   nn.Linear(256, 128), nn.ReLU())
-    self.projection = nn.Linear(args.n_filters * 6 + 128, 128)
-    self.out = nn.Linear(128,2)
+    self.projection = nn.Linear(args.n_filters * 6 , 128)
+  
+  def forward_short(self, x):
+    w_emb = self.word_embed(x[0]).transpose(1, 2)
+    c_emb = self.char_embed(x[1]).transpose(1, 2)
+
+    w_conv3 = F.relu(self.word_conv3_short(w_emb))
+    w_conv3 = F.max_pool1d(w_conv3, kernel_size=w_conv3.size()[-1])
+    w_conv4 = F.relu(self.word_conv4_short(w_emb))
+    w_conv4 = F.max_pool1d(w_conv4, kernel_size=w_conv4.size()[-1])
+    w_conv5 = F.relu(self.word_conv5_short(w_emb))
+    w_conv5 = F.max_pool1d(w_conv5, kernel_size=w_conv5.size()[-1])
+
+    c_conv3 = F.relu(self.char_conv3_short(c_emb))
+    c_conv3 = F.max_pool1d(c_conv3, kernel_size=c_conv3.size()[-1])
+    c_conv4 = F.relu(self.char_conv4_short(c_emb))
+    c_conv4 = F.max_pool1d(c_conv4, kernel_size=c_conv4.size()[-1])
+    c_conv5 = F.relu(self.char_conv5_short(c_emb))
+    c_conv5 = F.max_pool1d(c_conv5, kernel_size=c_conv5.size()[-1])
+
+    w_features = torch.cat([w_conv3, w_conv4, w_conv5], -2).squeeze()
+    c_features = torch.cat([c_conv3, c_conv4, c_conv5], -2).squeeze()
+    tmp = w_features + c_features
+    return tmp
+
+  def forward_long(self, x):
+    w_emb = self.word_embed(x[0]).transpose(1, 2)
+    c_emb = self.char_embed(x[1]).transpose(1, 2)
+
+    w_conv3 = F.relu(self.word_conv3_long(w_emb))
+    w_conv3 = F.max_pool1d(w_conv3, kernel_size=w_conv3.size()[-1])
+    w_conv4 = F.relu(self.word_conv4_long(w_emb))
+    w_conv4 = F.max_pool1d(w_conv4, kernel_size=w_conv4.size()[-1])
+    w_conv5 = F.relu(self.word_conv5_long(w_emb))
+    w_conv5 = F.max_pool1d(w_conv5, kernel_size=w_conv5.size()[-1])
+
+    c_conv3 = F.relu(self.char_conv3_long(c_emb))
+    c_conv3 = F.max_pool1d(c_conv3, kernel_size=c_conv3.size()[-1])
+    c_conv4 = F.relu(self.char_conv4_long(c_emb))
+    c_conv4 = F.max_pool1d(c_conv4, kernel_size=c_conv4.size()[-1])
+    c_conv5 = F.relu(self.char_conv5_long(c_emb))
+    c_conv5 = F.max_pool1d(c_conv5, kernel_size=c_conv5.size()[-1])
+
+    w_features = torch.cat([w_conv3, w_conv4, w_conv5], -2).squeeze()
+    c_features = torch.cat([c_conv3, c_conv4, c_conv5], -2).squeeze()
+    tmp = w_features + c_features
+    return tmp
+
 
   def forward(self, x):
     # x = [info, desc, short desc]
     info = x['info']
     desc = x['desc']
     short_desc = x['short_desc']
+    short_desc_feature = self.forward_short(short_desc)
+    long_desc_feature = self.forward_long(desc)
+    #prop_feature = self.prop_MLP(info.float())
 
-    short_desc_feature = self.short_desc_CNN(short_desc)
-    long_desc_feature = self.long_desc_CNN(desc)
-    prop_feature = self.prop_MLP(info.float())
-
-    feature = torch.cat([prop_feature, short_desc_feature, long_desc_feature], -1)
+    #feature = torch.cat([prop_feature, short_desc_feature, long_desc_feature], -1)
+    feature = torch.cat([short_desc_feature, long_desc_feature], -1)
     feature = F.relu(self.projection(feature))
-    out = self.out(feature)
+    return feature
+  
+  def gen(self, bug_ids):
+    batch_size = 64
+    num_batch = int(len(bug_ids) / batch_size)
+    if len(bug_ids) % batch_size > 0:
+      num_batch += 1
+    loop = tqdm(range(num_batch))
+    for i in loop:
+      batch_ids = []
+      for j in range(batch_size):
+        offset = batch_size * i + j
+        if offset >= len(bug_ids):
+          break
+        batch_ids.append(bug_ids[offset])
+      yield loop, read_batch_bugs(batch_ids, data = '../data/eclipse/', test = True)
+
+  def predict(self, bug_ids):
+    out = []
+    for _, x in self.gen(bug_ids):
+        out.extend(self.forward(x))
     return out
